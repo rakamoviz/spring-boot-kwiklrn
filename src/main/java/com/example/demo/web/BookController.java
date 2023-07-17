b33cad399e57b6ea0e480e0168fed8edaedddfbd
Cokorda Raka Angga Jananuraga <raka@deal-engine.com>
2023-07-17 02:51:52 -0600

    with integration test

--- src/main/java/com/example/demo/web/BookController.java
+++ src/main/java/com/example/demo/web/BookController.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Future;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,13 +21,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
-import org.springframework.scheduling.annotation.Async;
 
 import com.example.demo.persistence.model.Book;
-import com.example.demo.persistence.repo.BookRepository;
 import com.example.demo.service.BookService;
-import com.example.demo.web.exception.BookIdMismatchException;
-import com.example.demo.web.exception.BookNotFoundException;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -47,34 +42,45 @@ public class BookController {
     }
 
     @GetMapping("/title/{bookTitle}")
-    @Async
     public CompletableFuture<List<Book>> findByTitle(@PathVariable String bookTitle) {
       return bookService.findByTitle(bookTitle);
     }
 
     @GetMapping("/{id}")
-    @Async
     public CompletableFuture<Book> findOne(@PathVariable Long id) {
       return bookService.findOne(id);
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    @Async
     public CompletableFuture<Book> create(@Valid @RequestBody Book book) {
       return bookService.create(book); 
     }
 
     @DeleteMapping("/{id}")
-    @Async
     public CompletableFuture<Void> delete(@PathVariable Long id) {
       return bookService.delete(id);
     }
 
     @PutMapping("/{id}")
-    @Async
-    public CompletableFuture<Book> updateBook(@RequestBody Book book, @PathVariable Long id) {
+    public CompletableFuture<Book> updateBook(@Valid @RequestBody Book book, @PathVariable Long id) {
       return bookService.updateBook(book, id);
+    }
+
+    @PostMapping("/createandupdate")
+    @Transactional
+    public CompletableFuture<Book> createAndUpdate(@Valid @RequestBody Book book) {
+      return bookService.create(book).thenCompose(savedBook -> {
+        savedBook.setAuthor(savedBook.getAuthor() + " [updated]");
+        CompletableFuture<Book> res = bookService.updateBook(savedBook, savedBook.getId());
+        
+        if (book.getAuthor().equals("kaboom")) {
+          throw new RuntimeException("Kaboom");
+          //does not work, need mannual tx management
+        }
+        
+        return res;
+      });
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
